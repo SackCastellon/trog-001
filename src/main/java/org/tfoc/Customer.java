d1c50e9168ddc107e85b5d75af0d8cdf721c17c1
Juan José González Abril <jgonzab@mercadona.es>
2024-03-18 11:46:58 +0100

    refactor: Convert Rental class to a record

--- src/main/java/org/tfoc/Customer.java
+++ src/main/java/org/tfoc/Customer.java
@@ -29,30 +29,30 @@ public class Customer {
             double thisAmount = 0;
 
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
+            switch (each.movie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    if (each.daysRented() > 2)
+                        thisAmount += (each.daysRented() - 2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += each.daysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    if (each.daysRented() > 3)
+                        thisAmount += (each.daysRented() - 3) * 1.5;
                     break;
             }
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+            if ((each.movie().getPriceCode() == Movie.NEW_RELEASE) && each.daysRented() > 1)
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.movie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
 
