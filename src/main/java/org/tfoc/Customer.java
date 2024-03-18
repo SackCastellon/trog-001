25725dd4848470223d6a8ab1b25612dbffc82db6
Juan José González Abril <jgonzab@mercadona.es>
2024-03-18 11:51:11 +0100

    refactor: Add braces to if statements

--- src/main/java/org/tfoc/Customer.java
+++ src/main/java/org/tfoc/Customer.java
@@ -32,24 +32,25 @@ public class Customer {
             switch (each.movie().getPriceCode()) {
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.daysRented() > 2)
+                    if (each.daysRented() > 2) {
                         thisAmount += (each.daysRented() - 2) * 1.5;
+                    }
                     break;
                 case Movie.NEW_RELEASE:
                     thisAmount += each.daysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.daysRented() > 3)
-                        thisAmount += (each.daysRented() - 3) * 1.5;
+                    if (each.daysRented() > 3) {thisAmount += (each.daysRented() - 3) * 1.5;}
                     break;
             }
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.movie().getPriceCode() == Movie.NEW_RELEASE) && each.daysRented() > 1)
+            if ((each.movie().getPriceCode() == Movie.NEW_RELEASE) && each.daysRented() > 1) {
                 frequentRenterPoints++;
+            }
 
             // show figures for this rental
             result += "\t" + each.movie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
